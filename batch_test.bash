253765c9bc288078c3126410e89fd5fe3ff9fb7a
lee8950 <709064332@qq.com>
2024-06-22 17:30:24 +0800

    optimize

--- batch_test.bash
+++ batch_test.bash
@@ -1,6 +1,6 @@
-python run.py --dataset yelp --topk 20
-python run.py --dataset yelp --topk 40
-python run.py --dataset amazon --topk 20
-python run.py --dataset amazon --topk 40
-python run.py --dataset ml10m --topk 20
-python run.py --dataset ml10m --topk 40
+python run.py --dataset yelp --topk 20 --epoch 20
+python run.py --dataset yelp --topk 40 --epoch 20
+python run.py --dataset amazon --topk 20 --epoch 20
+python run.py --dataset amazon --topk 40 --epoch 20
+python run.py --dataset ml10m --topk 20 --epoch 20
+python run.py --dataset ml10m --topk 40 --epoch 20
